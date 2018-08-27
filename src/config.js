42873dfff3a17bf0162bc0f082d32a96703246ac
Shannon Canedy <git@mikidee.com>
2018-08-27 15:06:59 -0700

    fix auth url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_USER_URL = process.env.REACT_APP_API_USER_URL || "http://localhost:8080/api/users";
 export const API_AUTH_URL = process.env.REACT_APP_API_AUTH_URL || "http://localhost:8080/api/auth/login";
-export const API_BOARD_URL = process.env.REACT_APP_API_BOARD_URL || "http://localhost:8080/api/auth/board";
+export const API_BOARD_URL = process.env.REACT_APP_API_BOARD_URL || "http://localhost:8080/api/board";
 export const API_MAIN_BOARD_URL = process.env.REACT_APP_MAIN_API_BOARD_URL || "http://localhost:8080/api/board/5b7ce32657a288a4134d45a0";
 export const LANDING_PAGE = process.env.REACT_APP_LANDING_PAGE || "http://localhost:3000";
 export const BOARD_PAGE = process.env.REACT_APP_BOARD_PAGE || "http://localhost:3000/board";
